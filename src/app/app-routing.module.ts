f85908d9433f9194971dc0e70738dfcc21383b50
krisshna <krishna199987@gmail.com>
2021-07-21 22:05:04 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,17 +18,17 @@ const routes: Routes = [
   { path: 'translator',loadChildren:()=>import('./pages/home/home.module').then((m)=>m.HomeModule)},
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
 
-  {path:'chat',component:ChatComponent},
+
 
   // {path:'popup',component:PopupdetailsComponent},
   // {path:'covid-dashboard',component:UserdetailsComponent},
   // {path:'weather-dashboard',component:WeatherComponent},
-  {path:'mainadmin',component:MainadminComponent,canActivate:[TestCanActivate],canDeactivate:[TestCanDeactivate],children:[{path:'list',component:UserlistComponent},  {path:'mainusers',component:MainusersComponent},{path:'administrator',component:AdminComponent}]},
+  {path:'mainadmin',component:MainadminComponent,canActivate:[TestCanActivate],canDeactivate:[TestCanDeactivate],children:[{path:'list',component:UserlistComponent},  {path:'mainusers',component:MainusersComponent},{path:'administrator',component:AdminComponent},{path:'chat',component:ChatComponent}]},
   {path:'listings',component:UserlistComponent},
   { path: 'login', component: LoginComponent,
   children: [{ path: 'signup', component: SignupComponent } ]},
   { path:'User', component:UsersComponent,canActivate:[TestCanActivate],canDeactivate:[TestCanDeactivate],children:[ {path:'popup',component:PopupdetailsComponent},
-  
+  {path:'chat',component:ChatComponent},
   {path:'covid',component:UserdetailsComponent,},
   {path:'weather-dashboard',component:WeatherComponent},
   {path:'listings',component:UserlistComponent},
